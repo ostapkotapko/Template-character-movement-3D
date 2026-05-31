f31bcabed2964ed985c3b77c31a3ad3a3113c9bd
ostapkoTapko <ostapr73@gmail.com>
2026-05-31 22:43:35 +0200

    Revert "fix scene"
    This reverts commit f71c51601008af5712ac814f3222beaad87c0983.

---
+++ Assets/Project/Scripts/CameraSwitcher.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private Camera firstPersonCamera;
+    [SerializeField] private Camera thirdPersonCamera;
+    [SerializeField] private InputActionReference switchCameraAction;
+
+    private void OnEnable()
+    {
+        switchCameraAction.action.performed += OnSwitchCamera;
+    }
+
+    private void OnDisable()
+    {
+        switchCameraAction.action.performed -= OnSwitchCamera;
+    }
+
+    private void OnSwitchCamera(InputAction.CallbackContext context)
+    {
+        SwitchPersonCamera();
+    }
+
+    private void Start()
+    {
+        // Ensure only one camera is active at the start
+        firstPersonCamera.enabled = true;
+        thirdPersonCamera.enabled = false;
+    }
+
+    private void SwitchPersonCamera()
+    {
+        if (firstPersonCamera.enabled)
+        {
+            firstPersonCamera.enabled = false;
+            thirdPersonCamera.enabled = true;
+        }
+        else
+        {
+            firstPersonCamera.enabled = true;
+            thirdPersonCamera.enabled = false;
+        }
+    }
+}

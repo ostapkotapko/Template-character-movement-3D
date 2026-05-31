f5c6bc2429190ab70726d4560c6141ebc65f6bca
ostapkoTapko <ostapr73@gmail.com>
2026-05-31 23:10:00 +0200

    Add sprint
    Add sprint
    Add easy control of active steering modules

--- Assets/Project/Scripts/GameEvents.cs
+++ Assets/Project/Scripts/GameEvents.cs
@@ -3,12 +3,18 @@ using System;
 public static class GameEvents
 {
     public static event Action<bool> OnPlayerCanMove;
+    public static event Action<bool> OnPlayerCanSprint;
     public static event Action<bool> OnFirstCameraLook;
     public static event Action<bool> OnThirdCameraLook;
 
     public static void SetPlayerMoveState(bool canMove)
     {
         OnPlayerCanMove?.Invoke(canMove);
+    }
+
+    public static void SetPlayerSprintState(bool canSprint)
+    {
+        OnPlayerCanSprint?.Invoke(canSprint);
     }
 
     public static void SetFirstCameraLookState(bool canLook)
